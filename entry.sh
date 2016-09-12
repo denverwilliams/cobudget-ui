aa79dcbbdf8b8a20c590c6c6b329011913365b68
Hippie Hacker <chris@ii.org.nz>
2016-09-12 21:10:54 +0200

    Updating entry point to have env that we can override!

--- entry.sh
+++ entry.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+NPM_ENV = ${NPM_ENV:-development}
+API_PROTO = ${API_PROTO:-http://} 
+API_HOST = ${API_HOST:-localhost}
+API_PORT = ${API_PORT:-3000}
+API_URL = ${API_URL:-"${API_PROTO}${API_HOST}:${API_PART}/api/v1"}
+
+cat << ENV_FILE > config/${NPM_ENV}.js
+module.exports = {
+  apiPrefix: "${API_URL}"
+}
+ENV_FILE
+
 npm start
