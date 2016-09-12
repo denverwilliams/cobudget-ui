17bf9de390a42308e586607f1bb6e8a9b4b186ad
DLX <denver@ii.org.nz>
2016-09-12 22:46:45 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NPM_ENV="${NPM_ENV:-production}"
-API_PROTO="${API_PROTO:-http}"
-API_HOST="${API_HOST:-api.ii.org.nz}"
-API_PORT="${API_PORT:-80}"
-API_URL="${API_PROTO}://${API_HOST}:${API_PORT}/api/v1"
+export NPM_ENV="${NPM_ENV:-production}"
+export API_PROTO="${API_PROTO:-http}"
+export API_HOST="${API_HOST:-api.ii.org.nz}"
+export API_PORT="${API_PORT:-80}"
+export API_URL="${API_PROTO}://${API_HOST}:${API_PORT}/api/v1"
 
 cat << ENV_FILE > config/${NPM_ENV}.js
 module.exports = {
